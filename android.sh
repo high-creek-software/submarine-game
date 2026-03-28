8ba40ed3d1b25338b014d891d7c3043964c72179
Kendell Fabricius <kendell@highcreeksoftware.io>
2026-03-27 22:28:06 -0600

    Updated versions and build processes.

--- android.sh
+++ android.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ebitenmobile bind -target android -javapkg io.highcreeksoftware.submarinegame -o ./mobile/android/libs/submarinegame.aar ./cmd/mobile/
+ebitenmobile bind -target android -ldflags="max-page-size=16384" -javapkg io.highcreeksoftware.submarinegame -o ./mobile/android/libs/submarinegame.aar ./cmd/mobile/
