b3ee28c26b702186f867a4af443c233ce6598040
Kendell Fabricius <kendell@highcreeksoftware.io>
2026-04-06 20:38:42 -0600

    Working on updating the build for a new release.

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -2,8 +2,13 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
 }
 
 android {
@@ -36,11 +41,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_11
-        }
-    }
+
     buildFeatures {
         compose = true
     }
